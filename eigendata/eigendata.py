00580ba7532bd7422b56569511b8816da6de90b1
bjornaer <max@lumosidentity.com>
2023-03-07 20:47:16 -0300

    eigendata client initial commit

---
+++ eigendata/eigendata.py
@@ -0,0 +1,13 @@
+from eigendata.exceptions import raise_not_implemeneted
+
+
+def authenticate():
+    raise_not_implemeneted()
+
+
+def train():
+    raise_not_implemeneted()
+
+
+def predict():
+    raise_not_implemeneted()
