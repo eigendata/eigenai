fc4af30c9ab36a89d92a6cf30ca6eff392408789
Max Schulkin <maxemijo@gmail.com>
2023-03-08 09:23:02 -0300

    Bjornaer/add requests module (#5)
    * eigendata client initial commit
    
    * adding requests mdule and mocking methods
    
    * version bump
    
    * use urllib for requests

--- eigendata/eigendata.py
+++ eigendata/eigendata.py
@@ -1,18 +1,28 @@
+from typing import Optional
+
 from eigendata.exceptions import raise_not_implemeneted
+from eigendata.requests import post
 
 
 class Eigendata:
-    def __init__(self, api_token: str) -> None:
+    def __init__(self, api_token: str, api_url: Optional[str] = "b.eigendata.ai") -> None:
         self.token = api_token
+        self.api_url = api_url
 
-    def authenticate(self, *args, **kwargs):
-        raise_not_implemeneted()
+    def authenticate(self, username: str, password: str):
+        auth_url = f"{self.api_url}/token"
+        form_data = {"username": username, "password": password}
+        res = post(auth_url, data=form_data)
+        self.token = res.json()["access_token"]
 
-    def generate_rules(self, *args, **kwargs):
-        raise_not_implemeneted()
+    def generate_rules(self, data):
+        gen_rules_url = f"{self.api_url}/generate_rules"
+        headers = {"Content-Type": "application/json", "Authentication": f"Bearer {self.token}"}
+        body = {"data": data}
+        post(gen_rules_url, headers=headers, body=body)
 
     def predict(self, *args, **kwargs):
         raise_not_implemeneted()
 
-    def load_rule(self, *args, **kwargs):
+    def upload_rule(self, *args, **kwargs):
         raise_not_implemeneted()
